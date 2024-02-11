ad272c1e5ec1ed02cee45e0967cfbca41f2c3bc0
Susil25102007 <143895859+Susil25102007@users.noreply.github.com>
2024-02-11 16:19:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/Susil25102007/First--Class1.git /First--Class1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /First--Class1
 fi
-cd /TheMovieProviderBot
+cd /First--Class1
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting Kashmira...."
 python3 bot.py
